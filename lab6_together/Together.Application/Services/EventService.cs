1bec5ea8aae856135a66b25869209b320f8a853f
Mark Ho <mark@uuu.com>
2022-12-09 17:32:42 +0800

    增加了EventRepository與AddEvent

--- lab6_together/Together.Application/Services/EventService.cs
+++ lab6_together/Together.Application/Services/EventService.cs
@@ -1,10 +1,21 @@
+using Together.Application.Common.Interfaces;
+using Together.Domain.Entity;
+
 namespace Together.Application.Services;
 
 public class EventService : IEventService
 {
+    private readonly IEventRepository repository;
+
+    public EventService(IEventRepository repository)
+    {
+        this.repository = repository;
+    }
 
     public EventAddResult add(string name, string coordinator, string place, float lat, float lng, int fee)
     {
+        var event1 = new Event{Name = name, Coordinator=coordinator,Place = place, Lat=lat, Lng=lng, Fee=fee};
+        repository.AddEvent(event1);
         return new EventAddResult(Guid.NewGuid(), name, coordinator, place, lat, lng, fee);
     }
 
